55e19c84783ebedfcb096f98e3609d043b1810c9
urrik98 <adamrenschen@gmail.com>
2020-05-14 16:23:46 -0700

    Tweaks to Capfile and production.rb deployment config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -14,7 +14,7 @@ set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/var/www/html/rails/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{application}.sock"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/hlacovid19.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.access.log"
