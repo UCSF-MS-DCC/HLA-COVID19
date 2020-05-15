9effe7dc21dc732e2585614a98065d9a9fa5fe15
urrik98 <adamrenschen@gmail.com>
2020-05-15 10:45:33 -0700

    Update subject model-add relations

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,9 @@
 class Subject < ApplicationRecord
+    has_one :hla
+    has_one :baseline_c19_survey
+    has_many :weekly_c19_surveys
+    has_one :family
+    has_one :anxiety
+    has_one :smoking_history
+    has_one :medical_history
 end
