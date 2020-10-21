bf8dd1b3713bafc68eff3310d3183b2c5d325bc6
urrik98 <adamrenschen@gmail.com>
2020-10-20 17:08:53 -0700

    ...

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -3,12 +3,12 @@ class AdminMailer < ApplicationMailer
     layout 'mailer'
 
     def new_user_waiting_for_approval(user)
-        email = user.email
+        @email = user.email
         mail(to:'adam.renschen@ucsf.edu, covid.hla@gmail.com', subject: 'New HLA COVID19 registration awaiting approval')
     end
     def new_user_welcome_message(user)
-        email = user.email
-        mail(to:email, subject:"Thank you for registering for the HLA COVID19 database. The data governing team is evalutating your application and may contact you for additional information.")
+        @email = user.email
+        mail(to:@email, subject:"Thank you for registering for the HLA COVID19 database. The data governing team is evalutating your application and may contact you for additional information.")
     end
 
 end
