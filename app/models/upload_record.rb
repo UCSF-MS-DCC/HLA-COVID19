661cf91b39af4dd61852ce2cf87984ac2dc6e826
urrik98 <adamrenschen@gmail.com>
2020-10-23 14:10:11 -0700

    Add dependent destroy method to upload_records

--- app/models/upload_record.rb
+++ app/models/upload_record.rb
@@ -1,5 +1,5 @@
 class UploadRecord < ApplicationRecord
-  belongs_to :active_storage_attachment
+  belongs_to :active_storage_attachment, dependent: :destroy
   require 'tempfile'
 
   def get_report
