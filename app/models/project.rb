c4ce936c06145aba17d539361c7dd1f5d9f8a119
urrik98 <adamrenschen@gmail.com>
2020-11-17 16:25:09 -0800

    Reorder identifier columns in the get_* methods in project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -27,14 +27,14 @@ class Project < ApplicationRecord
   end
   def get_hla
     CSV.generate do |csv|
-        sub_col_heads = ["origin_identifier", "hlac19_id"]
+        sub_col_heads = ["hlac19_id", "origin_identifier"]
         hla_col_heads = Hla.column_names.reject{ |cn| ["id", "subject_id", "created_at", "updated_at"].include? cn }.map{ |cn| cn.to_s }
         full_col_heads = sub_col_heads.concat(hla_col_heads)
         subjects_ids = self.subjects.pluck(:id)
         hlas = Hla.where(subject_id:subjects_ids)
         csv << full_col_heads
         subjects_ids.each do |sid|
-            sub_vals = Subject.find(sid).attributes.values_at(*["origin_identifier", "hlac19_id"])
+            sub_vals = Subject.find(sid).attributes.values_at(*["hlac19_id", "origin_identifier"])
             hla_vals = Hla.find_by(subject_id:sid) ? Hla.find_by(subject_id:sid).attributes.values_at(*hla_col_heads) : nil
             row = (hla_vals.nil?) ? sub_vals : sub_vals.concat(hla_vals)
             csv << row
@@ -43,14 +43,14 @@ class Project < ApplicationRecord
   end
   def get_stats
     CSV.generate do |csv|
-        sub_col_heads = ["origin_identifier", "hlac19_id"]
+        sub_col_heads = ["hlac19_id", "origin_identifier"]
         stat_col_heads = Imputationstat.column_names.reject{ |cn| ["id", "hla_id", "created_at", "updated_at"].include? cn }.map{ |cn| cn.to_s }
         full_col_heads = sub_col_heads.concat(stat_col_heads)
         subjects_ids = self.subjects.pluck(:id)
         hlas = Hla.where(subject_id:subjects_ids)
         csv << full_col_heads
         hlas.each do |h|
-            sub_vals = h.subject.attributes.values_at(*["origin_identifier", "hlac19_id"])
+            sub_vals = h.subject.attributes.values_at(*["hlac19_id", "origin_identifier"])
             stat_vals = h.imputationstat.attributes.values_at(*stat_col_heads)
             row = sub_vals.concat(stat_vals)
             csv << row
@@ -59,13 +59,13 @@ class Project < ApplicationRecord
   end
   def get_comorbidities
     CSV.generate do |csv|
-        sub_col_heads = ["origin_identifier", "hlac19_id"]
+        sub_col_heads = ["hlac19_id", "origin_identifier"]
         comorb_col_heads = Comorbidity.column_names.reject{ |cn| ["id", "subject_id", "created_at", "updated_at"].include? cn }.map{ |cn| cn.to_s }
         full_col_heads = sub_col_heads.concat(comorb_col_heads)
         subjects_ids = self.subjects.pluck(:id)
         csv << full_col_heads
         subjects_ids.each do |sid|
-            sub_vals = Subject.find(sid).attributes.values_at(*["origin_identifier", "hlac19_id"])
+            sub_vals = Subject.find(sid).attributes.values_at(*["hlac19_id", "origin_identifier"])
             comorb_vals = Comorbidity.find_by(subject_id:sid) ? Comorbidity.find_by(subject_id:sid).attributes.values_at(*comorb_col_heads) : nil
             row = (comorb_vals.nil?) ? sub_vals : sub_vals.concat(comorb_vals)
             csv << row
@@ -74,13 +74,13 @@ class Project < ApplicationRecord
   end  
   def get_symptoms
     CSV.generate do |csv|
-        sub_col_heads = ["origin_identifier", "hlac19_id"]
+        sub_col_heads = ["hlac19_id", "origin_identifier"]
         c19_col_heads = C19Symptom.column_names.reject{ |cn| ["id", "subject_id", "created_at", "updated_at"].include? cn }.map{ |cn| cn.to_s }
         full_col_heads = sub_col_heads.concat(c19_col_heads)
         subjects_ids = self.subjects.pluck(:id)
         csv << full_col_heads
         subjects_ids.each do |sid|
-            sub_vals = Subject.find(sid).attributes.values_at(*["origin_identifier", "hlac19_id"])
+            sub_vals = Subject.find(sid).attributes.values_at(*["hlac19_id", "origin_identifier"])
             c19_vals = C19Symptom.find_by(subject_id:sid) ? C19Symptom.find_by(subject_id:sid).attributes.values_at(*c19_col_heads) : nil
             row = (c19_vals.nil?) ? sub_vals : sub_vals.concat(c19_vals)
             csv << row
@@ -89,13 +89,13 @@ class Project < ApplicationRecord
   end
   def get_hospitalizations
     CSV.generate do |csv|
-        sub_col_heads = ["origin_identifier", "hlac19_id"]
+        sub_col_heads = ["hlac19_id", "origin_identifier"]
         hosp_col_heads = Hospitalization.column_names.reject{ |cn| ["id", "subject_id", "created_at", "updated_at"].include? cn }.map{ |cn| cn.to_s }
         full_col_heads = sub_col_heads.concat(hosp_col_heads)
         subjects_ids = self.subjects.pluck(:id)
         csv << full_col_heads
         subjects_ids.each do |sid|
-            sub_vals = Subject.find(sid).attributes.values_at(*["origin_identifier", "hlac19_id"])
+            sub_vals = Subject.find(sid).attributes.values_at(*["hlac19_id", "origin_identifier"])
             hosp_vals = Hospitalization.find_by(subject_id:sid) ? Hospitalization.find_by(subject_id:sid).attributes.values_at(*hosp_col_heads) :nil
             row = (hosp_vals.nil?) ? sub_vals : sub_vals.concat(hosp_vals)
             csv << row
@@ -104,13 +104,13 @@ class Project < ApplicationRecord
   end
   def get_lab_tests
     CSV.generate do |csv|
-        sub_col_heads = ["origin_identifier", "hlac19_id"]
+        sub_col_heads = ["hlac19_id", "origin_identifier"]
         lb_col_heads = LabTest.column_names.reject{ |cn| ["id", "subject_id", "created_at", "updated_at"].include? cn }.map{ |cn| cn.to_s }
         full_col_heads = sub_col_heads.concat(lb_col_heads)
         subjects_ids = self.subjects.pluck(:id)
         csv << full_col_heads
         subjects_ids.each do |sid|
-            sub_vals = Subject.find(sid).attributes.values_at(*["origin_identifier", "hlac19_id"])
+            sub_vals = Subject.find(sid).attributes.values_at(*["hlac19_id", "origin_identifier"])
             lb_vals = LabTest.find_by(subject_id:sid) ? LabTest.find_by(subject_id:sid).attributes.values_at(*lb_col_heads) : nil
             row = (lb_vals.nil?) ? sub_vals : sub_vals.concat(lb_vals)
             csv << row
@@ -119,13 +119,13 @@ class Project < ApplicationRecord
   end
   def get_risk_factors
     CSV.generate do |csv|
-        sub_col_heads = ["origin_identifier", "hlac19_id"]
+        sub_col_heads = ["hlac19_id", "origin_identifier"]
         rf_col_heads = RiskFactor.column_names.reject{ |cn| ["id", "subject_id", "created_at", "updated_at"].include? cn }.map{ |cn| cn.to_s }
         full_col_heads = sub_col_heads.concat(rf_col_heads)
         subjects_ids = self.subjects.pluck(:id)
         csv << full_col_heads
         subjects_ids.each do |sid|
-            sub_vals = Subject.find(sid).attributes.values_at(*["origin_identifier", "hlac19_id"])
+            sub_vals = Subject.find(sid).attributes.values_at(*["hlac19_id", "origin_identifier"])
             rf_vals = RiskFactor.find_by(subject_id:sid) ? RiskFactor.find_by(subject_id:sid).attributes.values_at(*rf_col_heads) : nil
             row = (rf_vals.nil?) ? sub_vals : sub_vals.concat(rf_vals)
             csv << row
@@ -134,14 +134,14 @@ class Project < ApplicationRecord
   end
   def get_treatments
     CSV.generate do |csv|
-        sub_col_heads = ["origin_identifier", "hlac19_id"]
+        sub_col_heads = ["hlac19_id", "origin_identifier"]
         tmt_col_heads = Treatment.column_names.reject{ |cn| ["id", "subject_id", "created_at", "updated_at"].include? cn }.map{ |cn| cn.to_s }
         full_col_heads = sub_col_heads.concat(tmt_col_heads)
         subjects_ids = self.subjects.pluck(:id)
         tmts = Treatment.where(subject_id:subjects_ids)
         csv << full_col_heads
         subjects_ids.each do |sid|
-            sub_vals = Subject.find(sid).attributes.values_at(*["origin_identifier", "hlac19_id"])
+            sub_vals = Subject.find(sid).attributes.values_at(*["hlac19_id", "origin_identifier"])
             tmt_vals = Treatment.find_by(subject_id:sid) ? Treatment.find_by(subject_id:sid).attributes.values_at(*tmt_col_heads) : nil
             row = (tmt_vals.nil?) ? sub_vals : sub_vals.concat(tmt_vals)
             csv << row
