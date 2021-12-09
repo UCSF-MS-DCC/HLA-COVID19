76c4b36f01ca671439c3868095e3952a33037c3b
urrik98 <adamrenschen@gmail.com>
2021-12-09 11:31:24 -0800

    Update admin_mailer to resolve notification error

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,9 +2,10 @@ class AdminMailer < ApplicationMailer
     default from: 'admin@database-hlacovid19.org'
 
     def new_user_waiting_for_approval(user)
-        @email = user.email
-        @username = "#{user.firstname} #{user.lastname}"
-        @affiliation = user.affiliation
+        @user = User.find_by(email:user)
+        @email = @user.email
+        @username = "#{@user.firstname} #{@user.lastname}"
+        @affiliation = @user.affiliation
         mail(to:'adam.renschen@ucsf.edu, covid.hla@gmail.com', subject: 'New HLA COVID19 registration awaiting approval')
     end
     def new_user_welcome_message(user)
