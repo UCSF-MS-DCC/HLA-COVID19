ef95962388cffe4b10e648e7d5bb492032ef0b4a
Adam Renschen <adamrenschen@gmail.com>
2023-08-02 11:43:07 -0700

    Reran rails app:update, pushing changes to repo

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
+  devise_for :users
   root 'home#index'
 
   get 'home/auth'
