1c97f566cf42503e929f3c789fad2d07acd2c1bc
Adam Renschen <adamrenschen@gmail.com>
2023-08-02 11:04:16 -0700

    Fudge the activerecord version on three most recent database migration files.

--- db/migrate/20221227192609_change_highest_temp_to_decimal.rb
+++ db/migrate/20221227192609_change_highest_temp_to_decimal.rb
@@ -1,4 +1,4 @@
-class ChangeHighestTempToDecimal < ActiveRecord::Migration[6.1]
+class ChangeHighestTempToDecimal < ActiveRecord::Migration[5.2]
   def change
     change_column :c19_symptoms, :highest_temp, :decimal
   end
