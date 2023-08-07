2953adb613c5c28a05a51efca35c8a3baff7a487
Adam Renschen <adamrenschen@gmail.com>
2023-08-07 13:12:22 -0700

    Update environment configs to stop webpacker from running yarn integrity check

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,10 +7,7 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-#added block below to resolve yarn executable not found on server
-Rails.application.config.assets.configure do |env|
-  env.export_concurrent = false
-end
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
