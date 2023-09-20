3456d5b04300655678c7db6f4e59189a51ac69b7
Adam Renschen <adamrenschen@gmail.com>
2023-09-20 11:07:36 -0700

    Update application layout, home.js and application.js to address custom js not loading.

--- app/javascript/src/home.js
+++ app/javascript/src/home.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load',function(){
+$(document).on('turbo:load', () => {
 
   if (window.location.pathname === "/home/new_account_approval") {
     // when either an approve or remove button is clicked, disable the other option and send the params to the controller method.
