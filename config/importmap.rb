abe86d2c224dfc232bdc353cd9b0712fdb2afb31
Adam Renschen <adamrenschen@gmail.com>
2023-10-30 13:59:48 -0700

    More updates to javascript after hotwire install

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,4 +3,6 @@
 # pin "popper", to: "popper.js", preload: true
 # pin "bootstrap", to: "bootstrap.min.js", preload: true
 # pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-# pin_all_from "app/javascript/src", under: "src"
+# pin_all_from "app/javascript/src", under: "src"pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
