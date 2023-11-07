03e30340d97082b15b8b5e3d8e5cc042f145b89f
Adam Renschen <adamrenschen@gmail.com>
2023-11-07 15:28:22 -0800

    Removed old webpack files

--- config/webpack/environment.js
+++
@@ -1,5 +0,0 @@
-// const { environment } = require('@rails/webpacker')
-// environment.addExternals({
-//     "@hotwired/turbo-rails": "commonjs @hotwired/turbo-rails"
-// });
-// module.exports = environment
