9c62c5ee470b2da6fe0906079961902f39087b63
Adam Renschen <adamrenschen@gmail.com>
2023-11-07 10:18:18 -0800

    Update application.js: changed double quotes to single quotes.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,13 @@
 // Entry point for the build script in your package.json
-import { Turbo } from "@hotwired/turbo-rails"
+import { Turbo } from '@hotwired/turbo-rails'
 Turbo.session.drive = false
-import jquery from "jquery"
+import jquery from 'jquery'
 window.jQuery = jquery
-import "./src/home"
-import "./src/datatables"
-import "./src/application_prev"
-import "./src/project"
-import "./src/cable"
+import './src/home'
+import './src/datatables'
+import './src/application_prev'
+import './src/project'
+import './src/cable'
 //import * as bootstrap from "bootstrap"
 //import DataTable from "datatables.net-bs5"
-import "./controllers"
+import './controllers'
