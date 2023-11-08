a7eda390d66985a9879c7eb9236745dd5750cd59
Adam Renschen <adamrenschen@gmail.com>
2023-11-08 10:48:42 -0800

    Uncomment the link_tree ../builds directive in app/assets/config/menifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,7 @@
 //= link_tree ../images
 //= link_directory ../../javascript
 //= link_directory ../stylesheets
-// link_tree ../builds
+//= link_tree ../builds
 //= link application.css
 //= link custom.css
 //= link home.css
